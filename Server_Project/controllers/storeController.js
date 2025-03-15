3542d9c1018fcefb7e4a56681de7078f554ba1e6
Monkey3123 <leongtc@outlook.com>
2025-03-15 00:34:49 -0600

    To many Validators

--- Server_Project/controllers/storeController.js
+++ Server_Project/controllers/storeController.js
@@ -2,7 +2,7 @@ import Store from "../models/storeModel.js"; // Import Store model
 import jwt from "jsonwebtoken";
 import validator from "validator";
 import Factory from "./factory.js";
-import validator from "validator";
+
 // Function to create a JWT token
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "30d" });
